a95e6b436e1f593e4e3b48b05a18900e31182c4c
Ivan-dude <ivandude87@mail.ru>
2024-07-21 23:08:09 +1000

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,29 +1,14 @@
 def custom_write(file_name, info):
-    file1 = open(file_name, 'w') #открываем фаил для записи
-    tell_1_pos = file1.tell() #узнаем номера байта начала строки
-    file1.write(f'{info[0]}\n') #записываем первую строку
-    tell_2_pos = file1.tell()
-    file1 = open(file_name, 'a', encoding='utf-8')
-    file1.write(f'{info[1]}\n') #записываем след. строку
-    tell_3_pos = file1.tell()
-    file1.write(f'{info[2]}\n') #записываем след. строку
-    tell_4_pos = file1.tell()
-    file1.write(f'{info[3]}') #записываем след. строку
-    file1 = open(file_name, 'r', encoding='utf-8')
-    # for index, line in enumerate(file1, 1): #попробовал получить номера строк
-    #     print(index)
-    file1.close()
-    keys = [(1, tell_1_pos), (2, tell_2_pos), (3, tell_3_pos), (4, tell_4_pos)] #создал для удобства
-    strings_position = dict(zip(keys, info)) #собираем всё в словарь
-    return strings_position #выводим получившийся словарь
+    dict_ = {}
+    file = open(file_name, 'w', encoding='utf-8') #открываем фаил для записи
+    for i in range(len(info)):
+        dict_[(i + 1, file.tell())] = info[i]
+        file.write(info[i] + '\n') #записываем строки
+    file.close()
+    return dict_ #выводим словарь
+
 file_name = 'test.txt'
 info = ['Text for tell.', 'Используйте кодировку utf-8.', 'Because there are 2 languages!', 'Спасибо!']
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
-
-
-
-
-
-
+    print(elem)
